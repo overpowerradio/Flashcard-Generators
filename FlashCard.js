0401e0729ed4ecf7a6f498e7369d981b1184631c
Dj Will Power <Dj Will Power>
2017-07-21 07:58:15 -0400

    fixed the cloze card to present a new question with each loop.

--- FlashCard.js
+++ FlashCard.js
@@ -11,26 +11,28 @@ var method = new Basic("If a function is part of an object, what is it called?",
 var keys = new Basic("What do you call the name of the variables within an object?", "Keys");
 var values = new Basic("What is the other part of a key that makes up a property within an object?", "A value");
 
-var clozeList = [propertyC, methodC, keysC];
-var clozeCount = clozeList.length;
-
 var propertyC = new ClozeCard("If a variable is apart of an object, it's called a property.", "property");
-var methodC = new ClozeCard("If a function is called an [.......], when it's apart of an object", "method.");
-var keysC = new ClozeCard("The propertes of an object are variables that we call [......] to which you will then assign a value to.", "Keys");
+var methodC = new ClozeCard("A function is called a method, when it's apart of an object.", "method");
+var keysC = new ClozeCard("The propertes of an object are variables that we call keys to which you will then assign a value to.", "keys");
+
+
+var clozeList = [propertyC, methodC, keysC];
+var clozeCount = 0;
 
 
 
  function clozelearning() {
-    if (clozeCount > 0) {
-    
+    if (clozeCount < 3) {
+      var i = clozeCount;
+
       inquirer.prompt([
       {
         name: "question",
-        message: propertyC.text.replace(propertyC.cloze, "....")
+        message: clozeList[i].text.replace(clozeList[i].cloze, "....")
       },
         ]).then(function(answers) {
-        console.log("Full Text: " + propertyC.text);
-        clozeCount--;
+        console.log("\nThe correct answer: " + clozeList[i].cloze + "\n" + clozeList[i].text + "\n");
+        clozeCount++;
         clozelearning()
         });
     }  
@@ -51,7 +53,8 @@ function basiclearning() {
 		}  
 	}
 
- 
+ // console.log(clozeList[0].text.replace(clozeList[0].cloze, "...."));
+
 
 inquirer.prompt([
   {
