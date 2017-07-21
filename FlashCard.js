f87f1592901f8199a5bd9b5d57797cf1bfa32b64
Dj Will Power <Dj Will Power>
2017-07-21 08:12:58 -0400

    basic cards looping, but not responding to else/if

--- FlashCard.js
+++ FlashCard.js
@@ -2,14 +2,15 @@ var inquirer = require("inquirer");
 var ClozeCard = require("./ClozeCard.js");
 var Basic = require("./Basic.js");
 
-//Changes Made
-var basicCards = [property, method, keys, values];
-var basicCount = basicCards.length;
-
 var property = new Basic("If a variable is apart of an object, what is it called?", "A property");
 var method = new Basic("If a function is part of an object, what is it called?", "A method");
 var keys = new Basic("What do you call the name of the variables within an object?", "Keys");
 var values = new Basic("What is the other part of a key that makes up a property within an object?", "A value");
+
+var basicCards = [property, method, keys, values];
+var basicCount = 0;
+
+
 
 var propertyC = new ClozeCard("If a variable is apart of an object, it's called a property.", "property");
 var methodC = new ClozeCard("A function is called a method, when it's apart of an object.", "method");
@@ -22,7 +23,7 @@ var clozeCount = 0;
 
 
  function clozelearning() {
-    if (clozeCount < 3) {
+    if (clozeCount < clozeList.length) {
       var i = clozeCount;
 
       inquirer.prompt([
@@ -38,16 +39,17 @@ var clozeCount = 0;
     }  
   }
 function basiclearning() {
-		if (basicCount > 0) {
-		
+		if (basicCount < basicCards.length) {
+		var j = basicCount;
+
 			inquirer.prompt([
 			{
 				name: "question",
-				message: property.front
+				message: basicCards[j].front
 			},
 				]).then(function(answers) {
-				console.log("The correct answer is: " + property.back);
-        basicCount--;
+				console.log("The correct answer: " + basicCards[j].back + "\n");
+        basicCount++;
 				basiclearning()
 				});
 		}  
@@ -78,11 +80,11 @@ inquirer.prompt([
   }
 ]).then(function (answers) {
 
-	if (this.answers == 'Basic Card') {
+	if (this.answers = 'Basic Card') {
 				basiclearning();
 	}
 
-  else {
+  else if (this.answers = 'Cloze Card') {
     clozelearning();
   }
   // console.log(JSON.stringify(answers, null, '  '));
